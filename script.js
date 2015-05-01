272bf30cfecc3322efc281bcf8ebae9f5ca91b46
mliq <liquori@gmail.com>
2015-05-01 14:10:06 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+$(document).ready(function(){
+
+    $('.kittenLink').attr('href', 'http://placekitten.com/320/210');
+    $('.kittens').text('Click here to look at a kitten!!! OMG');
+    $('.kittenDescription').text('Meowzaa');
+    $('.kittenImg').attr('src', 'http://placekitten.com/320/210');
+
+});
