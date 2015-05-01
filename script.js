7eedbe1d4075f24ee272599a98296ffdffff78e0
mliq <liquori@gmail.com>
2015-05-01 15:14:50 -0500

    Search working with oAuth key

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ var userPage = null;
 var userDescription = null;
 var userImage = null;
 var query = null;
+var ourData = null;
 
 $(document).ready(function(){
 
     $('.searchBtn').on('click', function(){
         query = $('.searchField').val();
-        console.log(query);
 
         $.ajax({
             type: 'GET',
-            url: 'https://api.github.com/users/' + encodeURI(query) + ' -u mliq:hotdogyay1',
+            url: 'https://api.github.com/users/' + encodeURI(query) + '?client_id=f8a4b95805c9804c9eb7&client_secret=4b1bff35a5b8b802fe4bb4e1204afd2f56fc8d8d',
             crossDomain: true,
-            dataType: 'text',
+            dataType: 'json',
             success: function (data) {
                 console.log(data);
-
+                ourData = data;
                 username = data.login;
                 userPage = data.html_url;
                 userDescription = data.location;
