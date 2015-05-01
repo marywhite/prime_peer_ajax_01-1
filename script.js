a72d2decd8824082943250ad41fcfa84c454ce74
mliq <liquori@gmail.com>
2015-05-01 14:55:36 -0500

    Probably working but API has cut us off, added search functionality

--- script.js
+++ script.js
@@ -2,21 +2,39 @@ var username = null;
 var userPage = null;
 var userDescription = null;
 var userImage = null;
+var query = null;
 
 $(document).ready(function(){
 
-    $.get('https://api.github.com/users/mliq', function(data){
-        username = data.login;
-        userPage = data.html_url;
-        userDescription = data.location;
-        userImage = data.avatar_url
-        console.log(userDescription,userImage,username, userPage);
+    $('.searchBtn').on('click', function(){
+        query = $('.searchField').val();
+        console.log(query);
 
-        $('.username').text(username);
-        $('.userLinkUrl').attr('src',userPage);
-        $('.userLinkText').text(userPage);
-        $('.kittenDescription').text(userDescription);
-        $('.kittenImg').attr('src', userImage);
+        $.ajax({
+            type: 'GET',
+            url: 'https://api.github.com/users/' + encodeURI(query) + ' -u mliq:hotdogyay1',
+            crossDomain: true,
+            dataType: 'text',
+            success: function (data) {
+                console.log(data);
+
+                username = data.login;
+                userPage = data.html_url;
+                userDescription = data.location;
+                userImage = data.avatar_url;
+
+                $('.username').text(username);
+                $('.userLinkUrl').attr('src',userPage);
+                $('.userLinkText').text(userPage);
+                $('.kittenDescription').text(userDescription);
+                $('.kittenImg').attr('src', userImage);
+            },
+            error: function (xhr, status) {
+                alert('Error: ' + status);
+            }
+        });
+
     });
+
 
 });
